c7c6bf83e4d31f00cc9792d7f1021497bfee22ad
Rongsong Shen <rshen@shenrs.eu>
2026-09-20 16:41:29 +0800

    Wrap simplex with runtime dependencies (texlive, graphviz, gnuplot, imagemagick)

--- flake.nix
+++ flake.nix
@@ -12,9 +12,20 @@
         pkgs = import nixpkgs { inherit system; };
         haskellPackages = pkgs.haskellPackages;
 
+        # Tools simplex shells out to at runtime. simplex invokes these by
+        # bare name (pdflatex/xelatex/latexmk/lwarpmk/pdfcrop from texlive,
+        # dot from graphviz, gnuplot, and convert from imagemagick), so they
+        # must be present on PATH when the produced binary runs.
+        runtimeDeps = [
+          pkgs.texlive.combined.scheme-full
+          pkgs.graphviz
+          pkgs.gnuplot
+          pkgs.imagemagick
+        ];
+
         # callCabal2nix reads simplex.cabal directly, so dependencies stay
         # in sync with the cabal file automatically.
-        simplex = pkgs.haskell.lib.overrideCabal
+        simplexUnwrapped = pkgs.haskell.lib.overrideCabal
           (haskellPackages.callCabal2nix "simplex" ./. { })
           (_: {
             # The sources target an old mtl where Control.Monad.Cont
@@ -27,10 +38,24 @@
 import Control.Monad.IO.Class (liftIO)'
             '';
           });
+
+        # Wrap the binary so its runtime tools are always on PATH,
+        # regardless of the user's environment.
+        simplex = pkgs.runCommand "simplex-${simplexUnwrapped.version}"
+          {
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            inherit (simplexUnwrapped) meta;
+          }
+          ''
+            mkdir -p $out/bin
+            makeWrapper ${simplexUnwrapped}/bin/simplex $out/bin/simplex \
+              --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps}
+          '';
       in
       {
         packages = {
           simplex = simplex;
+          simplex-unwrapped = simplexUnwrapped;
           default = simplex;
         };
 
@@ -40,11 +65,11 @@ import Control.Monad.IO.Class (liftIO)'
         };
 
         devShells.default = haskellPackages.shellFor {
-          packages = _: [ simplex ];
+          packages = _: [ simplexUnwrapped ];
           nativeBuildInputs = with haskellPackages; [
             cabal-install
             ghc
-          ];
+          ] ++ runtimeDeps;
         };
       });
 }
